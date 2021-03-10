3bbf77cc6a1a353f55911b828d4b1cfbea27b499
Galaad-CoudercVanhove <galaad-28@orange.fr>
2021-03-10 15:25:12 +0100

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include "Course.cpp"
+
+using namespace std;
+
+int main(){
+    Course Course1 = afficher();
+    Course Course2 = afficher();
+    Course Course3 = afficher();
+}
