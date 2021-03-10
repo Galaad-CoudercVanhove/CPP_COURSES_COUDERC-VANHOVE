061c727ed1f0b7662ab89a7477b7cb4766ee28ea
Galaad-CoudercVanhove <galaad-28@orange.fr>
2021-03-10 14:44:13 +0100

    Initial commit

---
+++ Course.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+class Course{
+    private:
+        std::string _nomcourse;
+        std::string _nomjoueur;
+        int _score1;
+        int _score2;
+        int _score3;
+        int _bestscore;
+    public:
+        std::string getNomcourse();
+        std::string getNomjoueur();
+        float getScore1();
+        float getScore2();
+        float getScore3();
+        float getBestscore();
+
+};
