c345febca9f0451255dc257aa4f12fed98302c2f
Galaad-CoudercVanhove <galaad-28@orange.fr>
2021-03-10 15:25:26 +0100

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -2,3 +2,12 @@
 #include <string>
 #include <iostream>
 
+void Course::afficher(){
+    std::cout << Score1;
+    std::cout << Score2;
+    std::cout << Score3;
+}
+
+std::string Cousre::getNomjoueur(){
+    return _nomjoueur;
+}
